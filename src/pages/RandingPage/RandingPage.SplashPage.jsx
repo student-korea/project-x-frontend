00b2d731462e15d3ca8029183a6aaae93a989a70
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2025-09-05 18:06:14 +0900

    feat: 스플래시 편지 퍼블리싱, 슬라이딩 이벤트 구현

--- src/pages/RandingPage/RandingPage.SplashPage.jsx
+++ src/pages/RandingPage/RandingPage.SplashPage.jsx
@@ -5,6 +5,8 @@ function SplashPage() {
     const [hoverAllowed, setHoverAllowed] = useState(false);
     //  애니메이션이 끝났으면 true, 진행 중이면 false
     const [animated, setAnimated] = useState(false);
+    // 편지를 보여 주는 상태 여부
+    const [showLetter, setShowLetter] = useState(false);
 
     return(
         <itemS.SplashPageContainer>
@@ -15,10 +17,50 @@ function SplashPage() {
             onAnimationEnd={() => {
                 setHoverAllowed(true),
                 setAnimated(true)
-                }}>
+                }}
+            onClick={()=>setShowLetter(true)}
+                >
                 <itemS.TitleSubText>X, 무한한 가능성의 선택</itemS.TitleSubText>
                 <itemS.TitleMainText>Project-X</itemS.TitleMainText>
             </itemS.TitleWappper>
+            {showLetter && (
+                <itemS.LetterWrapper>
+                    <itemS.LetterTextContainer>
+                        <itemS.LetterTitleText>Fixer님께</itemS.LetterTitleText>
+                        <itemS.LetterContentText>
+                            {`
+                            안녕하세요. 
+                            우리는 지금 무한한 가능성과 선택의 세계 속을 헤매고 있는 작은 목소리들입니다.
+                            어느 길로 가야 할지, 누구의 손을 잡아야 할지 몰라 망설이는 중이에요.
+                            그래서, 당신의 손길이 필요합니다.`}
+                        </itemS.LetterContentText>
+                        <itemS.LetterContentText>
+                            {`
+                            당신은 이 세계에서 멤버를 고르고, 조합하고, 
+                            이야기를 엮어 데뷔 무대에 세울 수 있는 유일한 존재예요. 
+                            한 명의 성격, 한 번의 선택이 곧 그룹의 표정과 리듬이 됩니다.
+                            모든 가능성은 당신이 빚어낼 수 있는 재료일 뿐이에요.`}
+                            
+                        </itemS.LetterContentText>
+                        <itemS.LetterContentText>
+                            {`
+                            부담 없이 시작해 주세요. 
+                            그 선택이 누군가의 웃음이 되고, 누군가의 응원이 됩니다.
+                            Fixer님, 우리에게 첫 발을 내딛는 용기를 주세요.
+                            당신의 선택으로 이 작은 별들이 빛나게 될 거예요.`}
+                        </itemS.LetterContentText>
+                        <itemS.LetterFromTextWrapper>
+                            <itemS.LetterFromText>
+                                {`언제나 기다리며,
+                                Project - x 드림`}
+                            </itemS.LetterFromText>
+                        </itemS.LetterFromTextWrapper>
+                    </itemS.LetterTextContainer>   
+                    <itemS.BtnWrapper>
+                        <itemS.NextBtn>시작하기</itemS.NextBtn>
+                    </itemS.BtnWrapper>   
+                </itemS.LetterWrapper>
+            )}
         </itemS.SplashPageContainer>
     )
 }
--- src/pages/RandingPage/styled/RandingPage.SplashPage.style.js
+++ src/pages/RandingPage/styled/RandingPage.SplashPage.style.js
@@ -1,5 +1,4 @@
 import styled, { keyframes, css } from "styled-components";
-import { useState } from "react";
 
 // 점점 커지는 애니메이션
 const scaleUp = keyframes`
@@ -11,6 +10,7 @@ const scaleUp = keyframes`
     }
 `;
 
+// 작아지는 스케일 애니메이션
 const scaleDown = keyframes`
     from {
         transform:scale(1.05);
@@ -26,11 +26,25 @@ const fadeIn = keyframes`
     to { opacity: 1; }
 `;
 
+// 호버 시 커졌다 작아졌다 애니메이션
 const pulse = keyframes`
   0% { transform: scale(1); }
   50% { transform: scale(1.05); }
   100% { transform: scale(1); }
 `;
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(100%); /* 아래쪽에서 시작 */
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);    /* 제자리 */
+    opacity: 1;
+  }
+`;
+
+////////////////////////////////////
 
 export const SplashPageContainer = styled.div`
     width: 100vw;
@@ -58,23 +72,22 @@ export const TitleWappper = styled.div.attrs(() => ({}))`
     // ease-out: 점점 느리게 커지도록 자연스러운 느낌을 줌
     animation: ${pulse} 1s ease-in-out infinite;
 
-    ${({ animated }) => 
-        animated ? 
-        css`
+    ${({ animated }) =>
+        animated ?
+            css`
         /* animated === true 일 때 (진입한 뒤 첫 애니메이션 실행이 종료된 후) */
         transform: scale(1);  /* 애니메이션 없이 기본값 유지 */
       `
-    : css`
+            : css`
         /* animated === false 일 때 (처음 진입했을 때) */
         animation: ${scaleUp} 2.5s ease-out forwards;
         
       `
     }
-    
 
     ${({ hoverAllowed }) =>
-    hoverAllowed &&
-    css`
+        hoverAllowed &&
+        css`
         &:hover {
             /* 애니메이션이 종료된 후 호버하였을 때*/
             animation: ${pulse} 1s ease-in-out infinite;
@@ -86,15 +99,13 @@ export const TitleWappper = styled.div.attrs(() => ({}))`
     `}
 `;
 
-    
-
 export const TitleSubText = styled.span`
     color: #172031;
     opacity: 0;                       /* 처음엔 안 보임 */
     animation: ${fadeIn} 1s ease-out forwards;
     animation-delay: 2.5s;              /* 원이 커진 뒤 시작 */
     margin: 0;
-    `;
+`;
 
 export const TitleMainText = styled.h1`
     color: #172031;
@@ -102,4 +113,92 @@ export const TitleMainText = styled.h1`
     animation: ${fadeIn} 1s ease-out forwards;
     animation-delay: 2.5s;              /* 원이 커진 뒤 시작 */
     margin: 0;
-    `;
+`;
+
+export const LetterWrapper = styled.div`
+    width: 80%;
+    height: 80%;
+    position: absolute;
+    bottom: 0;
+    max-width: 708px;
+
+    border-radius: 25px 25px 0 0;
+    background: #FAFAF7;
+
+    box-shadow: 5px 4px 100px 0 rgba(0, 0, 0, 0.25);
+
+    animation: ${slideUp} 1s ease-in-out forwards;
+    padding: 50px;
+    
+`;
+
+export const LetterTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: space-between;
+    margin-bottom: 10px;
+`
+
+export const LetterTitleText = styled.p`
+    color: #172031;
+    //font-family: Pretendard;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    
+`;
+
+export const LetterContentText = styled.p`
+    color: #172031;
+    //font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-align: left;
+    white-space: pre-line;
+`;
+export const LetterFromTextWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: end;
+    margin: 50px 0 30px;
+`
+
+export const LetterFromText = styled.p`
+    color: #000;
+    //font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    white-space: pre-line;
+`;
+
+export const BtnWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const NextBtn = styled.div`
+    display: flex;
+    width: 496px;
+    height: 67px;
+    padding: 17px 107px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    border-radius: 15px;
+    background: #A259FF;
+
+    color: #FFF;
+    //font-family: Pretendard;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
