cefef815b631333b065a1fce15935a518a440a94
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2025-09-05 17:11:16 +0900

    chore: 상태 관련 주석 추가

--- src/pages/RandingPage/RandingPage.SplashPage.jsx
+++ src/pages/RandingPage/RandingPage.SplashPage.jsx
@@ -3,11 +3,15 @@ import { useState } from "react"
 function SplashPage() {
     // 호버 가능한 상태인가 (첫 실행때는 애니메이션이 끝나기 전까지 호버 액션 적용 x)
     const [hoverAllowed, setHoverAllowed] = useState(false);
+    //  애니메이션이 끝났으면 true, 진행 중이면 false
     const [animated, setAnimated] = useState(false);
 
     return(
         <itemS.SplashPageContainer>
-            <itemS.TitleWappper animated={animated} hoverAllowed={hoverAllowed}
+            <itemS.TitleWappper 
+            // props로 넘겨주는 상태
+            animated={animated} hoverAllowed={hoverAllowed}
+            // 첫 진입 후 애니메이션이 종료되면 실행
             onAnimationEnd={() => {
                 setHoverAllowed(true),
                 setAnimated(true)
