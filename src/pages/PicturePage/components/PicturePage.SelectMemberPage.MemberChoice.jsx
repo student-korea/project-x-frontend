45b056b21f277e089bfa22f64110edbb2b1e779f
ryuje1 <fbwodms2004@gmail.com>
2025-09-09 10:04:46 +0900

    feat: 멤버선택 페이지

---
+++ src/pages/PicturePage/components/PicturePage.SelectMemberPage.MemberChoice.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as itemS from "@/pages/PicturePage/styled/PicturePage.SelectMemberPage.style"
+
+function MemberChoice(){
+    return (
+        <itemS.choice>
+            같이 사진 찍을 멤버를 골라주세요!
+        </itemS.choice>
+    );
+}
+
+export default MemberChoice
